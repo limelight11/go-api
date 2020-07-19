8563a1c50db63b9aed085d7e8c5bb94c4f307acc
shota.noda <sn.limelight@gmail.com>
2020-07-19 19:32:59 +0900

    不要なコメントを削除

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
-# ベースとなるDockerイメージ指定
 FROM golang:latest
-# コンテナ内に作業ディレクトリを作成
+
 RUN mkdir /go/src/work
 RUN go get -u github.com/labstack/echo/...
 RUN go get bitbucket.org/liamstask/goose/cmd/goose
-# コンテナログイン時のディレクトリ指定
+
 WORKDIR /go/src/work
-# ホストのファイルをコンテナの作業ディレクトリに移行
+
 ADD . /go/src/work
--- server.go
+++ server.go
@@ -7,21 +7,16 @@ import (
 )
 
 func main() {
-  // Echo instance
   e := echo.New()
 
-  // Middleware
   e.Use(middleware.Logger())
   e.Use(middleware.Recover())
 
-  // Routes
   e.GET("/", hello)
 
-  // Start server
   e.Logger.Fatal(e.Start(":1323"))
 }
 
-// Handler
 func hello(c echo.Context) error {
   return c.String(http.StatusOK, "Hello, World!")
 }
